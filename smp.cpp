eaa00b622266f70c3a5d693245f62d2398fea3cb
Intrets <geen.inspiratie824@gmail.com>
2021-10-07 21:45:00 +0200

    initial

--- smp.cpp
+++ smp.cpp
@@ -1,12 +1,57 @@
-﻿// smp.cpp : Defines the entry point for the application.
-//
+﻿#include <iostream>
 
-#include "smp.h"
+#include <chrono>
+#include <string_view>
 
-using namespace std;
-
-int main()
+namespace smp
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	template<class F>
+	static void time(F f, std::string_view message) {
+		auto start = std::chrono::system_clock::now();
+		std::chrono::duration<double> d = std::chrono::duration<double>::zero();
+
+		size_t iteration = 0;
+		for (; iteration < 100000; iteration++) {
+			f();
+
+			auto time = std::chrono::system_clock::now();
+			d += time - start;
+
+			start = time;
+
+			if (d.count() > 5.0) {
+				break;
+			}
+		}
+
+		using namespace std::chrono;
+		double seconds = d.count() / iteration;
+		auto s = seconds;
+		auto ms = s * 1000.0;
+		auto us = ms * 1000.0;
+		auto ns = us * 1000.0;
+
+		double val;
+		std::string_view suffix;
+		if (ns < 1000.0) {
+			val = ns;
+			suffix = "ns";
+		}
+		else if (us < 1000.0) {
+			val = us;
+			suffix = "us";
+		}
+		else if (ms < 1000.0) {
+			val = ms;
+			suffix = "ms";
+		}
+		else {
+			val = s;
+			suffix = "s";
+		}
+
+		std::cout << "-------\n";
+		std::cout << message << "\n" << "iterations: " << iteration << " average: " << val << suffix<<"\n";
+		std::cout << "-------\n";
+	}
 }
